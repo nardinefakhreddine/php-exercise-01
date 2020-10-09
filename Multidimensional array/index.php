5c767f838159e87929a99cddcb0627bface50d95
Nardine <nardinefakhreddine@gmail.com>
2020-10-09 21:13:47 +0300

    part4

--- Multidimensional array/index.php
+++ Multidimensional array/index.php
@@ -1,10 +1,10 @@
 <?php
-$genres = array( "musicals" => array(0=>"Oklahoma", 1=>"The Music Man", 2=>"South Pacific"),
+$data= array( "musicals" => array(0=>"Oklahoma", 1=>"The Music Man", 2=>"South Pacific"),
 "dramas" => array(0=>"Lawrence of Arabia", 1=>"To Kill a Mockingbird", 2=>"Casablanca"),
 "mysteries" => array(0=>"The Maltese Falcon", 1=>"Rear Window", 2=>"North by Northwest"));
 
 
-foreach($genres as $x => $value){
+foreach($data as $x => $value){
     echo $x."<br>";
 foreach($value as $i=>$values){
     
