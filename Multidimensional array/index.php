bb3cd20e39185f3dbbc521673b493087791521b8
Nardine <nardinefakhreddine@gmail.com>
2020-10-09 21:11:28 +0300

    part-4

---
+++ Multidimensional array/index.php
@@ -0,0 +1,15 @@
+<?php
+$genres = array( "musicals" => array(0=>"Oklahoma", 1=>"The Music Man", 2=>"South Pacific"),
+"dramas" => array(0=>"Lawrence of Arabia", 1=>"To Kill a Mockingbird", 2=>"Casablanca"),
+"mysteries" => array(0=>"The Maltese Falcon", 1=>"Rear Window", 2=>"North by Northwest"));
+
+
+foreach($genres as $x => $value){
+    echo $x."<br>";
+foreach($value as $i=>$values){
+    
+    echo "-------> ".$i." = ".$values;
+    echo"<br>";
+}
+}
+?>
