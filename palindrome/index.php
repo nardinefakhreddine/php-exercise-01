fee22323b33614bddf1d39ca3fca81b43285c147
Nardine <nardinefakhreddine@gmail.com>
2020-10-09 23:37:34 +0300

    update palindrome

--- palindrome/index.php
+++ palindrome/index.php
@@ -1,10 +1,33 @@
 <?php
-function check_palindrome($string) 
+function check_palindrome1($string) 
 {
   if ($string == strrev($string))
-      return "Palindrom String";
+     return "Palindrom String";
+  else
+	  return "No Palindrom String";
+}
+echo check_palindrome1('nadam')."<br>";
+// autre methode
+function rev($string) {
+$length = strlen($string);  
+$res="";
+for ($i=($length-1) ; $i >= 0 ; $i--)   
+{  
+  $res.=$string[$i];  
+}  
+return $res;
+}
+$string = "JAVATPOINT"; 
+$rev=rev($string);
+echo $rev;
+echo "<br>";
+function check_palindrome($string) 
+{
+  if ($string == rev($string))
+     return "Palindrom String";
   else
 	  return "No Palindrom String";
 }
 echo check_palindrome('madam')."<br>";
-?>
+
+?>  
