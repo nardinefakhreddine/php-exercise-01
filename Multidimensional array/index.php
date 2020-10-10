662ae493d56e7a14c8a82e4ff78970294c8dee0c
Nardine <nardinefakhreddine@gmail.com>
2020-10-10 20:19:30 +0300

    complete part 4

--- Multidimensional array/index.php
+++ Multidimensional array/index.php
@@ -12,4 +12,39 @@ foreach($value as $i=>$values){
     echo"<br>";
 }
 }
+
+
+//Sorting the multidimensional associative array by the genres: <br />";
+function compare($x, $y)
+{ 
+if ( $x[1] == $y[1] )
+return 0;
+else if ( $x[1] > $y[1] )
+return -1;
+else
+return 1;
+}
+function compare2($x2, $y2) {
+if ($x[2] == $y[2])
+return 0;
+elseif ($x[2] > $y[2])
+return -1;
+else
+return 1;
+}
+usort($data, 'compare');
+usort($data, 'compare2');
+echo "Printing the multidimensional associative array sorted: <br>";
+foreach($data as $x => $value){
+    echo $x."<br>";
+foreach($value as $i=>$values){
+    
+    echo "-------> ".$i." = ".$values;
+    echo"<br>";
+}
+}
+
 ?>
+
+
+
